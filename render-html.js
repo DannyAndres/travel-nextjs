6fb05bf63c65af6b98f1ed577a5cf3dc2dea94a6
dannyandres <danny.aguilar@usach.cl>
2023-08-06 04:30:58 -0400

    [wip] Testing Coverage script

--- render-html.js
+++ render-html.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-
 async function run() {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({ headless: "new" });
         const page = await browser.newPage();
         await page.goto('file://' + path.join(__dirname, '/coverage/lcov-report/index.html'));
         await page.screenshot({ path: path.join(__dirname, '/screenshot.png') });
